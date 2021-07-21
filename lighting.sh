0d13f0ab1d77e9a415f5f1c9a1bdc92bbe5a5efa
rajathbharadwaj <rajathrajath940@gmail.com>
2021-07-21 13:48:21 +0530

    added shs

---
+++ lighting.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Running lights 90"
+python /Users/rajathdb/D/audio-reactive-led-strip/python/visualization.py --ipa 90
+
+
+
---
+++ lighting150.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Running lights150"
+python /Users/rajathdb/D/audio-reactive-led-strip/python/visualization.py --ipa 150
+
+
